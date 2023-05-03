97f05ed96165fb99d601a72b442899469c9fe8d6
Nathan Clover <cloverww04@gmail.com>
2023-05-02 19:02:43 -0500

    setup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
 // import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import React from 'react';
 
 function Home() {
   // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
